9b2e3608693fb277967e484195312c7e565fdaff
abdulsameer01 <abdulsameer0910@gmail.com>
2025-07-24 13:05:50 +0530

    Add URL Shortener React frontend project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UrlShortener from "./pages/UrlShortener";
+import RedirectPage from "./pages/RedirectPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<UrlShortener />} />
+        <Route path="/:shortId" element={<RedirectPage />} />
+      </Routes>
+    </Router>
   );
 }
 
